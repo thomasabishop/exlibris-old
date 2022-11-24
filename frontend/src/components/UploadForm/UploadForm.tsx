6538d7ff8e08f7ba0787f2a36290fe56c3db9736
tactonbishop <tactonbishop@gmail.com>
2022-11-24 19:30:11 +0000

    feature (FE): complete first vers of upload form

--- frontend/src/components/UploadForm/UploadForm.tsx
+++ frontend/src/components/UploadForm/UploadForm.tsx
@@ -1,18 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Form } from "react-bootstrap"
 import "./UploadForm.scss"
 import { bookCategories } from "../../data/testData"
 import { useForm, SubmitHandler } from "react-hook-form"
 import IBookEntry from "../../types/IBookEntry"
+import ToastMessage from "../ToastMessage"
 
 const UploadForm: React.FC = () => {
+   const [loading, setLoading] = useState<boolean>(false)
+   const [showToast, setShowToast] = useState<boolean>(false)
+
+   const bookCategoryIsValid = (selection: string): boolean => {
+      return bookCategories.some((x) => x.category === selection)
+   }
+
    const {
       register,
       handleSubmit,
       formState: { errors },
    } = useForm<IBookEntry>()
 
-   const onSubmit: SubmitHandler<IBookEntry> = (data) => console.log(data)
+   const onSubmit: SubmitHandler<IBookEntry> = (data) => {
+      const submission = () => {
+         setLoading(false)
+         setShowToast(true)
+         console.log(data)
+      }
+
+      setLoading(true)
+      setTimeout(submission, 2000)
+   }
 
    return (
       <Form className="UploadForm" onSubmit={handleSubmit(onSubmit)} noValidate>
@@ -37,46 +54,114 @@ const UploadForm: React.FC = () => {
                {...register("author", { required: true })}
                name="author"
                type="text"
-               className="form-control"
+               className={`form-control ${errors.title ? "invalid" : ""}`}
                placeholder="Emily Bronte"
                required
             />
+            {errors.author && (
+               <p className="UploadForm--feedback">Required field</p>
+            )}
          </div>
 
          <div className="mb-3 form-group">
             <label className="form-label">Category</label>
-            <select className="form-select" aria-label="Default select example">
+            <select
+               {...register("category", {
+                  required: true,
+                  validate: (v) => bookCategoryIsValid(v),
+               })}
+               className="form-select"
+               aria-label="Default select example"
+            >
+               <option selected disabled>
+                  Choose category
+               </option>
                {bookCategories.map((category, i) => (
                   <option key={i} value={category.category}>
                      {category.category}
                   </option>
                ))}
             </select>
+            {errors.category?.type === "required" && (
+               <p className="UploadForm--feedback">Required field</p>
+            )}
+            {errors.category?.type === "validate" && (
+               <p className="UploadForm--feedback">
+                  Please select a category or create a new one.
+               </p>
+            )}
          </div>
 
          <div className="mb-3 form-group">
-            <label className="form-label">Publication date</label>
+            <label className="form-label">Publication year</label>
             <input
-               {...register("pubDate", { required: true })}
+               {...register("pubDate", {
+                  required: true,
+                  maxLength: 4,
+                  minLength: 4,
+                  pattern: /[0-9]{4}/,
+               })}
                name="pubDate"
                type="text"
-               className="form-control"
+               className={`form-control ${errors.pubDate ? "invalid" : ""}`}
                placeholder="1847"
             />
+            {errors.pubDate?.type === "required" && (
+               <p className="UploadForm--feedback">Required field</p>
+            )}
+            {errors.pubDate?.type === "pattern" && (
+               <p className="UploadForm--feedback">
+                  Value must be 4-digit year: <em>YYYY</em>
+               </p>
+            )}
+            {errors.pubDate?.type === "minLength" && (
+               <p className="UploadForm--feedback">
+                  Value must be 4-digit year: <em>YYYY</em>
+               </p>
+            )}
+            {errors.pubDate?.type === "maxLength" && (
+               <p className="UploadForm--feedback">
+                  Value must be 4-digit year: <em>YYYY</em>
+               </p>
+            )}
          </div>
+
          <div className="mb-3 form-group">
-            <label className="form-label">Year of reading</label>
+            <label className="form-label">Reading date</label>
             <input
-               {...register("readDate", { required: true })}
+               {...register("readDate", {
+                  required: true,
+               })}
                name="readDate"
-               type="text"
-               className="form-control"
-               placeholder="2017"
+               type="date"
+               className={`form-control ${errors.readDate ? "invalid" : ""}`}
             />
+            {errors.readDate?.type === "required" && (
+               <p className="UploadForm--feedback">Required field</p>
+            )}
          </div>
-         <Button type="submit" className="btn btn-dark" variant="primary">
-            Add book
-         </Button>
+
+         {loading ? (
+            <Button className="btn btn-dark" variant="primary" disabled>
+               <span
+                  className="spinner-border spinner-border-sm mr-3"
+                  style={{ marginRight: "0.5rem" }}
+                  role="status"
+                  aria-hidden="true"
+               ></span>
+               Add book
+            </Button>
+         ) : (
+            <Button type="submit" className="btn btn-dark" variant="primary">
+               Add book
+            </Button>
+         )}
+
+         <ToastMessage
+            message="Book added to index"
+            showToast={showToast}
+            setShowToast={setShowToast}
+         />
       </Form>
    )
 }
--- frontend/src/pages/AddBook.tsx
+++ frontend/src/pages/AddBook.tsx
@@ -3,7 +3,10 @@ import UploadForm from "../components/UploadForm"
 const AddBook: React.FC = () => {
    return (
       <React.Fragment>
-         <h1 className="h2 pb-3 pt-1">Add book</h1>
+         <h1 className="h2  pt-1">Add book</h1>
+         <p className="lead">
+            Enter publication details manually or search for book
+         </p>
          <UploadForm />
       </React.Fragment>
    )
