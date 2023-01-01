49be1270c0dc0f24d25a926ec2551077a2ee2513
tactonbishop <tactonbishop@gmail.com>
2023-01-01 14:43:46 +0000

    feature(BE): add update and delete book functionality

--- backend/src/controllers/books-controller.js
+++ backend/src/controllers/books-controller.js
@@ -5,14 +5,36 @@ exports.list = async (req, res) => {
   res.send(books)
 }
 
-exports.create = async (req, res) => {
-  let book = new Books({
+exports.create = (req, res) => {
+  const book = new Books({
     title: req.body.title,
     author: req.body.author,
     genre: req.body.genre,
     publicationYear: req.body.publicationYear,
     dateRead: req.body.dateRead,
   })
-  await book.save()
+  book.save()
   res.send(book)
+}
+
+// TODO: Add better error handling here, distinguish not found from other errors
+exports.update = (req, res) => {
+  Books.findByIdAndUpdate(req.params.id, req.body, (err, updatedEntry) => {
+    if (err) {
+      res.status(404).send(err)
+    } else {
+      res.send(updatedEntry)
+    }
+  })
+}
+
+// TODO: Add better error handling here, distinguish not found from other errors
+exports.delete = (req, res) => {
+  Books.findByIdAndDelete(req.params.id, (err, deletedEntry) => {
+    if (err) {
+      res.status(404).send(err)
+    } else {
+      res.send(deletedEntry)
+    }
+  })
 }
--- backend/src/routes/books-routes.js
+++ backend/src/routes/books-routes.js
@@ -3,10 +3,16 @@ const booksController = require("../controllers/books-controller")
 const validate = require("../validators/books-validators")
 const router = express.Router()
 
-// List all books in the database
+// List all documents in the books collection
 router.get("/", booksController.list)
 
-// Add a book to the database
+// Add a document to the books collection
 router.post("/", validate.book, booksController.create)
+
+// Update the properties of a document in the books collection
+router.put("/:id", booksController.update)
+
+// Delete a document from the books collection
+router.delete("/:id", booksController.delete)
 
 module.exports = router
